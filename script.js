7fe4e4b4c2d9e1d002900a002a5688144b19b0a4
jujopico <sujunqt@gmail.com>
2020-01-08 22:17:43 -0500

    fix achors didnt follow margin-top command

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 $('.navbar-toggler').on('click', function(){
   $(this).toggleClass('menu-open');
 })
+
+window.addEventListener("hashchange", function () {
+  window.scrollTo(window.scrollX, window.scrollY - 230);
+});
