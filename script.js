aa637d39612638db686f89f8ef1d21cdd2c825d5
jujopico <sujunqt@gmail.com>
2020-01-15 13:38:58 -0500

    script for back-to-top btn

--- script.js
+++ script.js
@@ -2,8 +2,28 @@ $('.navbar-toggler').on('click', function(){
   $(this).toggleClass('menu-open');
 })
 
-window.addEventListener("hashchange", function () {
-  window.scrollTo(window.scrollX, window.scrollY - 150);
-});
-window.addEventListener("hashchange", offsetAnchor);
-window.setTimeout(offsetAnchor, 1);
+// nav to right section if navbar is fixed
+// window.addEventListener("hashchange", function () {
+//   window.scrollTo(window.scrollX, window.scrollY - 150);
+// });
+// window.addEventListener("hashchange", offsetAnchor);
+// window.setTimeout(offsetAnchor, 1);
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
