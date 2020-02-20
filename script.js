81cd8a305c8a72a19aca2255237ab8494b2cfa22
jujopico <sujunqt@gmail.com>
2020-02-20 11:48:59 -0500

    delete unnecessary id

--- script.js
+++ script.js
@@ -11,6 +11,7 @@ $('.navbar-toggler').on('click', function(){
 
 //Get the button
 const mybutton = document.getElementById("myBtn");
+
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
